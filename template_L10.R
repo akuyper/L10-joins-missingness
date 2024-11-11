15e9286ab00c00753f0313f86e520b383fecb929
akuyper <a-kuyper@northwestern.edu>
2024-11-11 02:36:58 -0600

    update fall 2024

--- template_L10.R
+++ template_L10.R
@@ -1,9 +1,9 @@
-# L10 - joins and missingness ----
+# L10: joins and missingness ----
 # Stat 301-1
 
-## load packages ----
+# load packages ----
 
 
-## Exercises ----
+# Exercises ----
 
-### Ex 1 ----
+## Ex 1 ----
